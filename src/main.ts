7a77a07f73c0f6c8d90037aa2a04f3ab98b592c4
Carlo Zottmann <carlo@zottmann.org>
2023-06-07 16:34:53 +0200

    [NEW] Adds plugin version to start up log message

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Plugin } from "obsidian";
 import { ConsoleProxy } from "./console-proxy";
 import { NoteLogger } from "./note-logger";
+import { PLUGIN_INFO } from "./plugin-info";
 
 export default class LoggingNote extends Plugin {
   private logger: NoteLogger;
@@ -11,7 +12,10 @@ export default class LoggingNote extends Plugin {
     this.logger.log("_tableheader");
     this.cProxy = new ConsoleProxy({ app: this.app, logger: this.logger });
     this.cProxy.setup();
-    this.logger.log("info", this.logger.prefixMsg("Proxy set up"));
+    this.logger.log(
+      "info",
+      this.logger.prefixMsg(`Proxy set up (v${PLUGIN_INFO.pluginVersion})`),
+    );
   }
 
   onunload() {
