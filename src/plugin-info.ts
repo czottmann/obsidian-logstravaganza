ce07bc9da62c50014854862ba2221de5d6d87e2f
Carlo Zottmann <carlo@zottmann.dev>
2026-09-24 19:48:20 +0200

    chore(release): prepare version 2.4.0
    Align the plugin manifest, package metadata, and runtime version information for the next release.

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,5 +1,5 @@
 /* File will be overwritten by bin/release.sh! */
 export const PLUGIN_INFO = {
-  "pluginVersion": "2.3.0",
+  "pluginVersion": "2.4.0",
   "pluginReleasedAt": "2025-11-18T13:38:38+0100"
 }
