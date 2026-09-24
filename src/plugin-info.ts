ec5ae0a33426b20f3fb8b0314de135fccb83a6da
Carlo Zottmann <carlo@zottmann.dev>
2026-09-24 20:35:31 +0200

    [REL] Release 2.4.0

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,5 +1,5 @@
 /* File will be overwritten by bin/release.sh! */
 export const PLUGIN_INFO = {
   "pluginVersion": "2.4.0",
-  "pluginReleasedAt": "2025-11-18T13:38:38+0100"
+  "pluginReleasedAt": "2026-09-24T20:35:28+0200"
 }
